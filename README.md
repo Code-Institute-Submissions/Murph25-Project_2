c25f484e8f6b4b9f8261c39197231f1a1a9f113f
Murph25 <rebm_90@yahoo.ie>
2023-04-17 13:09:13 +0000

    added favicon to the site

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # **Rock Paper Scissors**
 ---
-Rock Paper Scissors is a website that allows users to play a simple rock paper scissors game and incudes instructions.
+Rock, paper scissors is a simple fun game in which both the players have to pick a rock, paper, or scissors. It has three possible outcomes: a draw, player wins or a computer wins. We will be designing the game using JavaScript where a player will be playing against the computer. The game is smply formatted and includes an instructions icon to support players understanding.Live site available here. 
 **site images**
+## Features:
+- Site wide:
+
+
--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -53,26 +53,18 @@ function getResult() {
   resultDisplay.innerHTML = result
 }
 
-// Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -83,11 +75,23 @@ function incrementScore() {
   let oldScore = parseInt(document.getElementById("score").innerText);
   document.getElementById("score").innerText = ++oldScore;
 }
-/**
-* Gets the current tally of incorrect answers from the DOM and increments it by 1
-*/
 function incrementWrongAnswer() {
 
   let oldScore = parseInt(document.getElementById("incorrect").innerText);
   document.getElementById("incorrect").innerText = ++oldScore;
+}
+function playRound() {
+  if (playerSelection === computerSelection) {
+    userScore++;
+    computerScore++;
+  } else if ((playerSelection === choises.rock && computerSelection === choises.scissors) ||
+    (playerSelection === choises.rock && computerSelection === choises.rock) ||
+    (playerSelection === choises.scissors && computerSelection === choises.paper)) {
+    userScore++;
+  } else {
+    computerScore++;
+  }
+
+  scoreWindow.innerHTML = userScore + ' : ' + computerScore;
+  return (computerScore, userScore);
 }
